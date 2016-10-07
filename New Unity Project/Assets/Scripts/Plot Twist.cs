03642d87850bc42b3f319f4d616d62bd1ebab92e
Evan Schoenberger <evanjschoenberger@gmail.com>
2016-10-07 16:46:15 -0400

    Added something to every folder so BEN WOULD SHUT UP.

---
+++ New Unity Project/Assets/Scripts/Plot Twist.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+// HERES' TEH PLOT TWIST: THIS DOES NOTHING.
+// @Author Evan
+public class PlotTwist : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
